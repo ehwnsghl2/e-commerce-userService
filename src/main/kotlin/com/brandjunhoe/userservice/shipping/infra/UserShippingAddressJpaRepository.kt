cf2d8f0e55f71c457a09a577cd0ce55da6c211b8
dojunhoe <ehwnsghl2@naver.com>
2022-05-22 17:17:45 +0900

    버그수정

--- src/main/kotlin/com/brandjunhoe/userservice/shipping/infra/UserShippingAddressJpaRepository.kt
+++ src/main/kotlin/com/brandjunhoe/userservice/shipping/infra/UserShippingAddressJpaRepository.kt
@@ -3,7 +3,6 @@ package com.brandjunhoe.userservice.shipping.infra
 import com.brandjunhoe.userservice.shipping.domain.UserShippingAddress
 import com.brandjunhoe.userservice.shipping.domain.UserShippingAddressRepository
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
 
-interface UserShippingAddressJpaRepository : JpaRepository<UserShippingAddress, UUID>, UserShippingAddressRepository {
+interface UserShippingAddressJpaRepository : JpaRepository<UserShippingAddress, Long>, UserShippingAddressRepository {
 }
